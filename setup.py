113d9b62a02af265d97bea72ca598776e48c3602
pitrodanishtha09 <165004253+pitrodanishtha09@users.noreply.github.com>
2026-01-31 14:33:51 +0530

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+## this will be used in installing machine learning application as a package.
+## with the use of that deploy in PyPi
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''this function will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='MLPROJECT',
+    version='0.0.1',
+    author='Nishtha',
+    author_email='pitrodanishtha09@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+## setup.py will consider this src as a package itself. You can import this just like how we import seaborn.
