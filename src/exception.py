e2e71e058e2d34590373fe2faca0842d8360ef60
pitrodanishtha09 <165004253+pitrodanishtha09@users.noreply.github.com>
2026-02-05 12:18:44 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 # custom exception handling in python
 
 def error_message_detail(error,error_detail:sys):
@@ -20,4 +20,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
