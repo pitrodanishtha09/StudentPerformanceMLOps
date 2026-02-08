71871a407f0d02be4e22c10e3e136d7c535d0d67
pitrodanishtha09 <165004253+pitrodanishtha09@users.noreply.github.com>
2026-02-08 22:20:22 +0530

    for deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,19 +8,24 @@ from src.utils import load_object
 
 class PredictPipeline:
     def __init__(self):
-        pass
-
-    def predict(self, features):
+        """
+        Load model and preprocessor once during initialization.
+        This avoids loading them on every request (prevents timeouts).
+        """
         try:
             model_path = os.path.join("artifacts", "model.pkl")
             preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
 
-            model = load_object(file_path=model_path)
-            preprocessor = load_object(file_path=preprocessor_path)
+            self.model = load_object(file_path=model_path)
+            self.preprocessor = load_object(file_path=preprocessor_path)
 
-            data_scaled = preprocessor.transform(features)
-            preds = model.predict(data_scaled)
+        except Exception as e:
+            raise CustomException(e, sys)
 
+    def predict(self, features):
+        try:
+            data_scaled = self.preprocessor.transform(features)
+            preds = self.model.predict(data_scaled)
             return preds
 
         except Exception as e:
